4d6db7afe0d1646f50ef0e331661072888ff21e9
Sahil Subhash badhe <sahilbadhe680@gmail.com>
2026-05-13 11:31:17 +0530

    Fix formatting in fetch URL call

--- my-app/src/Components/ProfileSetup.jsx
+++ my-app/src/Components/ProfileSetup.jsx
@@ -37,11 +37,10 @@ const ProfileSetup = () => {
         formData.append('profileImage', photo);
       }
 
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_SERVER_URL}/api/register`
-        , {
-          method: 'POST',
-          body: formData,
-        });
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_SERVER_URL}/api/register`, {
+        method: 'POST',
+        body: formData,
+      });
 
       const data = await response.json();
 
@@ -82,7 +81,8 @@ const ProfileSetup = () => {
                   className="hidden"
                 />
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707a1 1 0 01-.707.293H4zm12 12H4a4 4 0 01-4-4V9a1 1 0 012 0v8a2 2 0 002 2h12a2 2 0 002-2V9a1 1 0 112 0v8a4 4 0 01-4 4z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M10 4.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zM7 7a3 3 0 116 0 3 3 0 01-6 0z" clipRule="evenodd" />
                 </svg>
               </label>
             </div>
